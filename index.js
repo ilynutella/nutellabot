00a13ab6ce634f2ef43c98b380c0bc51cfc1c7f3
ilynutella <2ak2ak@naver.com>
2020-11-11 16:35:46 +0900

    bot upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const token = process.env.token;
+
+client.on('ready', () => {
+  console.log('켰다.');
+});
+
+client.on('message', (message) => {
+  if(message.content === '블바') {
+    message.channel.send('픽보');
+  }
+});
+
+client.on('message', (message) => {
+  if(message.content === '누멍텔') {
+    message.channel.send('청라이');
+  }
+});
+
+client.on('message', (message) => {
+  if(message.content === '블바') {
+    message.channel.send('픽보');
+  }
+});
+client.login(token);
