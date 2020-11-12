ff6b120231caf3cdd83308fac6e148a93ec85488
ilynutella <2ak2ak@naver.com>
2020-11-12 16:32:16 +0900

    bot upload

--- index.js
+++ index.js
@@ -35,4 +35,16 @@ client.on('message', (message) => {
     message.channel.send('돼지쉑');
   }
 });
+
+client.on('message', (message) => {
+  if(message.content === '!블픽') {
+    message.channel.send('https://www.youtube.com/channel/UC20D6TQnGxeMxfdx50Ss4-A');
+  }
+});
+
+client.on('message', (message) => {
+  if(message.content === '!누텔라') {
+    message.channel.send('https://www.youtube.com/channel/UCrxaG_2J0-cO4FYJsqousug? : 본계\nhttps://www.youtube.com/channel/UC3NobfBM8juUVytevO2NjIw? : 마인크래프트 채널\nhttps://www.youtube.com/channel/UCq26FktMJWSHH8Jpgvt1D7A? : 부계');
+  }
+});
 client.login(token);
